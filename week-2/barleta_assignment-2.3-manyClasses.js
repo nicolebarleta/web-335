bd107ab51b17839c534010aac805f8e0e8485eb3
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-10-26 15:13:47 -0500

    commit

---
+++ week-2/barleta_assignment-2.3-manyClasses.js
@@ -0,0 +1,26 @@
+const nodeTech = {
+
+    student: [{
+        _id:"S202021247",
+        first_name: "Marie",
+        last_name: "Barleta",
+        mi: "N"
+    }],
+
+        enroll: [{
+            _id:"330H",
+            grade: "2nd year",
+
+                classes:[
+                {   
+                    _id:"335",
+                    name:"NoSQL",
+                },
+
+                {   
+                    _id:"420",
+                    name:"Restful API's",
+
+                }]
+                     }]
+}
---
+++ week-2/barleta_assignment-2.3-manyStudents.js
@@ -0,0 +1,36 @@
+const nodeTech = {
+
+    class: [{
+        _id: "335",
+        name: "NoSql"
+    }],
+
+        enroll: [{
+            _id:"330H",
+            grade: "7th grade",
+
+                students:[
+                {   
+                    _id:"S202021247",
+                    first_name: "Marie",
+                    last_name: "Barleta",
+                    mi: "N"
+                },
+                
+                {
+                    _id:"S202021348",
+                    first_name: "Alex",
+                    last_name: "Resendiz",
+                    mi: "N/A"
+                },
+
+                {
+                    _id:"S202021477",
+                    first_name: "Elsa",
+                    last_name: "Barleta",
+                    mi: "R"
+                }
+        
+                ]
+        }]
+}
