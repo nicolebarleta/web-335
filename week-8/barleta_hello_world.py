77da6094189c124eecc072eeade5434ca85e3bf3
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-12-04 21:17:01 -0600

    Adding coding attributions

--- week-8/barleta_hello_world.py
+++ week-8/barleta_hello_world.py
@@ -1,1 +1,13 @@
+"""
+============================================
+; Title:  Exercise 8.2
+; Author: Professor Krasso
+; Date: 04 December 2020
+; Modified By: Marie Nicole Barleta
+; Description:  Print Hello World
+;===========================================
+
+"""
+
+
 print('Hello World')
