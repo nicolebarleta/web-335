5d045ec856134b9ad7d306e92b831a64a169c188
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-12-14 18:22:28 -0600

    Adding update document

--- week-9/barleta-user-service.py
+++ week-9/barleta-user-service.py
@@ -27,10 +27,10 @@ db = client.web335
 
 #creating a new user to be inserted
 newUser = {
-    "first_name": "Alex",
-    "last_name": "Arr",
-    "email": "alexarr@ar.wow",
-    "employee_id": "00000012",
+    "first_name": "Marie Nicole",
+    "last_name": "Barleta",
+    "email": "nicolebarleta@nb.wow",
+    "employee_id": "0000002",
     "date_created":datetime.datetime.utcnow()
 }
 
@@ -44,4 +44,4 @@ print("User: ",user_id ," Successfully added!")
 #prints a prompt that says details of found user
 print("Details of found user: ") 
 #findOne query to find the employee_id with 00000011, prints the found user
-pprint.pprint(db.users.find_one({"employee_id": "00000011"}))
+pprint.pprint(db.users.find_one({"employee_id": "0000001"}))
---
+++ week-9/barleta-user-update.py
@@ -0,0 +1,49 @@
+"""
+============================================
+; Title:  Exercise 9.3 - Updating and Deleting Documents
+; Author: Professor Krasso
+; Date: 14 December 2020
+; Modified By: Marie Nicole Barleta
+; Description:  Updating and Deleting documents
+;===========================================
+
+"""
+
+#importing from pymongo and mongoclient
+
+from pymongo import MongoClient
+
+#importing pretty print
+import pprint
+
+#importing date time module
+import datetime
+
+#conneting to localhost mongodb
+client = MongoClient('localhost', 27017)
+
+#connecting to web335 database
+db = client.web335
+ 
+
+#update query on user's email 
+db.users.update_one(
+
+    {"employee_id": "0000002"},
+
+    {
+
+        "$set": {
+
+            "email": "mnbarleta@my365.bellevue.edu"
+
+        }
+
+    }
+
+)
+
+#pretty print of employee_id with 0000002 and prints the details with user's email, employee_id, first_name, last_name
+
+print("Updated email of user: ")
+pprint.pprint(db.users.find_one({"employee_id": "0000002" } ,{"email": 1, "employee_id": 1, "first_name": 1, "last_name": 1} ))
